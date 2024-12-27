3ff366c35377d05118297f04adaca9d2e27ed99a
Ehte22 <syedehteshamali22@gmail.com>
2024-12-27 19:43:50 +0530

    update again

--- server/utils/messageConsumer.ts
+++ server/utils/messageConsumer.ts
@@ -1,35 +1,3 @@
-// import { channel } from "../services/rabbitMQ.service";
-
-// export const MSG_CONSUMER = (queue: string, correlationId: string): Promise<any> => {
-//     return new Promise<any>((resolve, reject) => {
-//         if (!channel) {
-//             reject(new Error("RabbitMQ channel is not initialized"));
-//             return;
-//         }
-
-//         channel.assertQueue(queue, { durable: true });
-
-//         channel.consume(queue, (msg) => {
-//             if (msg && msg.content) {
-//                 try {
-//                     const data = JSON.parse(msg.content.toString());
-
-//                     if (msg.properties.correlationId === correlationId) {
-//                         channel.ack(msg);
-//                         resolve(data);
-//                     } else {
-//                         channel.nack(msg, false, true);
-//                     }
-//                 } catch (error) {
-//                     console.error('Error processing message', error);
-//                     channel.nack(msg, false, false);
-//                     reject(error);
-//                 }
-//             }
-//         });
-//     });
-// };      
-
 import { Channel } from "amqplib";
 
 export const MSG_CONSUMER = (channel: Channel, queue: string, correlationId: string, timeoutMs = 5000): Promise<any> => {
